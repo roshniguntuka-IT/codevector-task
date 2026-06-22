0a093ef137cece2c051d0cf41be52ae6a1aa27ea
roshniguntuka-IT <roshniguntuka1814@gmail.com>
2026-06-22 20:14:41 +0530

    initial commit

---
+++ seed.js
@@ -0,0 +1,59 @@
+const pool = require("./db");
+
+const categories = [
+  "Electronics",
+  "Books",
+  "Clothing",
+  "Sports",
+  "Home"
+];
+
+async function seedProducts() {
+  try {
+    const totalProducts = 200000;
+    const batchSize = 1000;
+
+    for (let batchStart = 1; batchStart <= totalProducts; batchStart += batchSize) {
+
+      const values = [];
+
+      for (
+        let i = batchStart;
+        i < batchStart + batchSize && i <= totalProducts;
+        i++
+      ) {
+        const category =
+          categories[Math.floor(Math.random() * categories.length)];
+
+        const price =
+          Math.floor(Math.random() * 10000) + 100;
+
+        values.push(
+          `('Product ${i}', '${category}', ${price}, NOW(), NOW())`
+        );
+      }
+
+      const query = `
+        INSERT INTO products
+        (name, category, price, created_at, updated_at)
+        VALUES
+        ${values.join(",")}
+      `;
+
+      await pool.query(query);
+
+      console.log(
+        `Inserted ${Math.min(batchStart + batchSize - 1, totalProducts)} products`
+      );
+    }
+
+    console.log("Finished inserting 200,000 products");
+
+  } catch (error) {
+    console.error(error);
+  } finally {
+    await pool.end();
+  }
+}
+
+seedProducts();
---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const pool = require("./db");
+
+const app = express();
+app.get("/products", async (req, res) => {
+    try {
+        const { category, cursor, limit = 20 } = req.query;
+
+        let query = `
+            SELECT *
+            FROM products
+        `;
+
+        let values = [];
+
+        if (category) {
+            query += ` WHERE category = $1`;
+            values.push(category);
+        }
+
+        if (cursor) {
+            const position = values.length + 1;
+
+            query += category
+                ? ` AND id < $${position}`
+                : ` WHERE id < $${position}`;
+
+            values.push(cursor);
+        }
+
+        query += `
+            ORDER BY created_at DESC, id DESC
+            LIMIT ${Number(limit)}
+        `;
+
+        const result = await pool.query(query, values);
+
+       res.json({
+    products: result.rows,
+    nextCursor: result.rows.length
+        ? result.rows[result.rows.length - 1].id
+        : null
+});
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Database Error");
+    }
+});
+
+app.listen(3000, () => {
+    console.log("Server started on port 3000");
+});
